87474c3d7c82e58e75b5a385c610d2b58c45da31
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2024-11-22 16:12:07 +0600

    get all bycle products

--- src/modules/products/productsController.ts
+++ src/modules/products/productsController.ts
@@ -18,22 +18,22 @@ const createConProduct = async (req: Request, res: Response) => {
         })
     }
 }
-const getConProduct = async (req: Request, res: Response) => {
-    try {
-        const result = await productServices.getProducts()
-        res.status(200).send({
-            success: true,
-            messsage: 'Bicycles retrieved successfully',
-            data: result
-        })
-    } catch (err) {
-        res.status(500).send({
-            success: false,
-            messsage: 'Validation failed',
-            err
-        })
-    }
-}
+// const getConProduct = async (req: Request, res: Response) => {
+//     try {
+//         const result = await productServices.getProducts()
+//         res.status(200).send({
+//             success: true,
+//             messsage: 'Bicycles retrieved successfully',
+//             data: result
+//         })
+//     } catch (err) {
+//         res.status(500).send({
+//             success: false,
+//             messsage: 'Validation failed',
+//             err
+//         })
+//     }
+// }
 
 export const productController = {
     createConProduct,

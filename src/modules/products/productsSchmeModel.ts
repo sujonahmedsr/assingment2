0d7504ae904f88125ae373dadffc6c4ff693ff66
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2024-11-22 18:05:38 +0600

    createdAt and updatedAt working using google help

--- src/modules/products/productsSchmeModel.ts
+++ src/modules/products/productsSchmeModel.ts
@@ -3,20 +3,40 @@ import { productInterface } from "./productsInterface";
 
 // product schema create with interface
 const productSchema = new Schema<productInterface>({
-    name: { type: String, required: [true, 'Name field is required'] },
-    brand: { type: String, required: [true, 'Brand field is required'] },
-    price: { type: Number, required: [true, 'Price field is required'] },
+    name: {
+        type: String,
+        required: [true, 'Name field is required']
+    },
+    brand: {
+        type: String,
+        required: [true, 'Brand field is required']
+    },
+    price: {
+        type: Number,
+        required: [true, 'Price field is required']
+    },
     type: {
-        type: String, enum: {
+        type: String,
+        enum: {
             values: ['Mountain', 'Road', 'Hybrid', 'BMX', 'Electric'],
             message: '{VALUE} is not valid'
         },
-        required: [true, 'Type field is required'] 
+        required: [true, 'Type field is required']
     },
-    description: { type: String, required: [true, 'Description field is required'] },
-    quantity: { type: Number, required: [true, 'Quantity field is required'] },
-    inStock: { type: Boolean, required: [true, 'InStock field is required'] }
-})
+    description: {
+        type: String,
+        required: [true, 'Description field is required']
+    },
+    quantity: {
+        type: Number,
+        required: [true, 'Quantity field is required']
+    },
+    inStock: {
+        type: Boolean,
+        required: [true, 'InStock field is required']
+    },
+
+}, { timestamps: true })
 
 // create model 
 export const productModel = model<productInterface>('products', productSchema)
